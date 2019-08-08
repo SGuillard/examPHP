96d52a33783db97bfd5a380e405a469528fea305
sylvain.guillard <sylvain.guillard@ieseg.fr>
2019-08-08 19:42:39 +0200

    add mysql

--- public/index.php
+++ public/index.php
@@ -1,4 +1,15 @@
 <?php
 
+try {
+    $bdd = new PDO('mysql:host=test_mysql:3306;dbname=test;charset=utf8', 'root', 'root');
+    $reponse = $bdd->query("SELECT * FROM ok");
+    foreach ($reponse as $rep) {
+        var_dump($rep['col']);
+        die;
+    }
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
 echo date_default_timezone_get();
 die;
