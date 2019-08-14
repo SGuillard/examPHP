99feb4aed7835125313a0aae0c3354e93a3be268
sylvain.guillard <sylvain.guillard@ieseg.fr>
2019-08-14 10:34:54 +0200

    add node v3

--- public/index.php
+++ public/index.php
@@ -1,15 +1,14 @@
 <?php
 
 try {
-    $bdd = new PDO('mysql:host=test_mysql:3306;dbname=test;charset=utf8', 'root', 'root');
-    $reponse = $bdd->query("SELECT * FROM ok");
-    foreach ($reponse as $rep) {
-        var_dump($rep['col']);
-        die;
+    $conn = new mysqli('myapp_mysql:3306', 'root', 'root');
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
+    echo "Connected successfully" . PHP_EOL;
 } catch (Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
-
-echo date_default_timezone_get();
 die;
