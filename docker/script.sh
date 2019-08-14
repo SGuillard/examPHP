845507fa90eadebb358116dbc6e8983bf3dad05a
sylvain.guillard <sylvain.guillard@ieseg.fr>
2019-08-14 10:24:29 +0200

    Add Node

--- docker/script.sh
+++ docker/script.sh
@@ -1,16 +1,17 @@
 
 start_dev(){
     cp .env.dev .env
+	# because sometimes host can't find docker-sync
     source ~/.bashrc
     docker-sync start
 	docker-compose up -d
-	docker-compose exec php composer self-update --no-interaction
-	docker-compose exec php composer install --no-interaction
-	docker-compose exec php bin/console doctrine:migration:migrate --allow-no-migration --no-interaction
-	docker-compose exec php bin/console assets:install --symlink --relative
-	docker-compose exec php rm -Rf var/cache
-	docker-compose exec php bin/console cache:warmup
-	docker-compose exec php chmod -R 777 var
+	# docker-compose exec php composer self-update --no-interaction
+	# docker-compose exec php composer install --no-interaction
+	# docker-compose exec php bin/console doctrine:migration:migrate --allow-no-migration --no-interaction
+	# docker-compose exec php bin/console assets:install --symlink --relative
+	# docker-compose exec php rm -Rf var/cache
+	# docker-compose exec php bin/console cache:warmup
+	# docker-compose exec php chmod -R 777 var
     rm .env
 }	
 
@@ -27,36 +28,23 @@ start_prod(){
 }
 
 purge(){
+	cp .env.dev .env
     docker-sync stop
 	docker-sync-stack clean
-}
-	
-server_start(){
-    docker-sync start
-	docker-compose up -d
-}
-
-server_stop(){
-    docker-compose stop
-	@docker-sync stop
-}	
-
-new_migration(){
-    docker-compose exec php bin/console doctrine:migration:diff --no-interaction
-}
-	
-php_cli(){
-    docker-compose exec php bash
+	rm .env
 }
 	
 stop_dev(){
 cp .env.dev .env
+	docker-sync stop
+	docker-sync-stack clean
     docker stop $(docker ps -a -q) | xargs docker rm
 rm .env
 }
 
 stop_prod(){
 cp .env.prod .env
+	docker-compose stop
     docker stop $(docker ps -a -q) | xargs docker rm
 rm .env
 }
